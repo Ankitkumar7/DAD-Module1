73dde7ea8048c787579b44b76a7f2586d0d7333d
Ankit kumar <ankit@btech.christuniversity.in>
2017-01-23 13:40:07 +0000

    Add files via upload

---
+++ codes/Transmitter/transmitter/transmitter.ino
@@ -0,0 +1,31 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+const int ledPin = 13;      // led connected to digital pin 13
+const int knockSensor = A0; // the piezo is connected to analog pin 0
+const int threshold = 100;  // threshold value to decide when the detected sound is a knock or not
+
+
+int sensorReading = 0;      // variable to store the value read from the sensor pin
+int ledState = LOW; 
+void setup()
+{
+    Serial.begin(9600);   // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+   sensorReading = analogRead(knockSensor);
+  if (sensorReading >= threshold) {
+    const char *msg = "Hello Bijeesh Sir!";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(1000);
+}
+
+
+
+}
